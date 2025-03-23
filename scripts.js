44386ee07e972b99b74da9764fe79e95c45950e2
Mitchellzhou1 <95938232+Mitchellzhou1@users.noreply.github.com>
2025-03-22 22:44:41 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const games = [
+        {
+            name: 'League of Legends',
+            description: 'A fast-paced, competitive online game.',
+            image: 'images/league-of-legends.jpg',
+            link: 'https://www.leagueoflegends.com'
+        },
+        {
+            name: 'VALORANT',
+            description: 'A tactical first-person shooter.',
+            image: 'images/valorant.jpg',
+            link: 'https://www.riotgames.com/en/games/valorant'
+        },
+        {
+            name: 'Aicles',
+            description: 'An immersive role-playing game set in a mystical world.',
+            image: 'images/arcane-chronicles.jpg',
+           
+::contentReference[oaicite:2]{index=2}
+ 
+
